71bea61110483e292c4d9030e9592cb380315e7a
DoraHermes <hermes.dora@mayo.edu>
2020-03-28 18:16:10 -0500

    added baselinesubtraction

--- functions/ieeg_baselinesubtract.m
+++ functions/ieeg_baselinesubtract.m
@@ -1,14 +1,15 @@
-function data_epoch = ecog_baselinesubtract(data_epoch,t_base)
+function data_epoch = ieeg_baselinesubtract(data_epoch,t_base)
 
 % data_epoch = ecog_baselinesubtract(data_epoch,t_base)
 % subtracts the mean signal during t_base in each epoch
 % data_epoch = electrodes X epoch X t
+% dhermes, multimodal neurimaging lab, 2020
 
 % baseline correct
-for k=1:size(data_epoch,1)%channels
-    for m=1:size(data_epoch,2)%epochs
-        x=squeeze(data_epoch(k,m,:));
-        x=x-mean(x(t_base));
-        data_epoch(k,m,:)=x;
+for kk = 1:size(data_epoch,1)%channels
+    for mm = 1:size(data_epoch,2)%epochs
+        x = squeeze(data_epoch(kk,mm,:));
+        x = x-mean(x(t_base));
+        data_epoch(kk,mm,:) = x;
     end
 end
