da81044999903cc1728b0e04cca0efc5778a6add
DoraHermes <hermes.dora@mayo.edu>
2020-04-21 14:42:06 -0500

    LICENSE

--- functions/ieeg_carRegress.m
+++ functions/ieeg_carRegress.m
@@ -19,7 +19,7 @@ ca_signal = mean(signal(chans2incl,:),1);
 
 % regress off the mean signal
 for kk = 1:size(signal,1) % elecs
-    disp(['elec ' int2str(kk)])
+%     disp(['elec ' int2str(kk)])
     if ismember(kk,chans2incl)
         [~,~,R] = regress(signal(kk,:)',ca_signal');
         signal(kk,:) = R';
