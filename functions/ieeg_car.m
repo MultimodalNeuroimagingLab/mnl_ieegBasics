267cdf1b5e7ba3ea9b335adce10a51345141c6a6
DoraHermes <hermes.dora@mayo.edu>
2020-05-25 09:45:49 -0500

    fix typo

--- functions/ieeg_car.m
+++ functions/ieeg_car.m
@@ -1,4 +1,4 @@
-function [signalOut,spatfiltmatrix] = ecog_Car(signal, chans2incl)
+function [signalOut,spatfiltmatrix] = ecog_car(signal, chans2incl)
 
 % This function performs Common Avregae Reference (CAR) filtering on a
 % signal
