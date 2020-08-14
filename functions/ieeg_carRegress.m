6d711c3877204fe4dbe43280bc7aff9b8920a031
Max <m.a.vandenboom84@gmail.com>
2020-08-14 11:20:57 -0500

    Added optional silencing of output; formatted code a bit

--- functions/ieeg_carRegress.m
+++ functions/ieeg_carRegress.m
@@ -1,28 +1,30 @@
-function [signal] = ieeg_carRegress(signal, chans2incl)
 % common average referencing flunction with regressing out the mean, rather
 % than subtracting out the mean
 %
-% [signal] = ieeg_CarRegress(signal, chans2incl)
+% [signal] = ecog_carRegress(signal, chans2incl)
 %
 % signal = electrodes X samples
 % chans2incl: channels to include in CAR
 %
 % dh - Oct 2010
+% 
+function [signal] = ieeg_carRegress(signal, chans2incl, silent)
+    if exist('silent', 'var') == 0,  silent = 0;     end
 
-
-if size(signal,2) < size(signal,1) % signal samples X electrodes
-    disp('transpose signal to be electrodes X samples')
-    return
-end
-
-ca_signal = mean(signal(chans2incl,:),1);
-
-% regress off the mean signal
-for kk = 1:size(signal,1) % elecs
-%     disp(['elec ' int2str(kk)])
-    if ismember(kk,chans2incl)
-        [~,~,R] = regress(signal(kk,:)',ca_signal');
-        signal(kk,:) = R';
+    if size(signal,2) < size(signal,1) % signal samples X electrodes
+        disp('transpose signal to be electrodes X samples')
+        return
     end
-end
 
+    ca_signal = mean(signal(chans2incl,:),1);
+
+    % regress off the mean signal
+    for kk = 1:size(signal,1) % elecs
+        if silent == 0, disp(['elec ' int2str(kk)]);    end
+        if ismember(kk,chans2incl)
+            [B,BINT,R] = regress(signal(kk,:)',ca_signal');
+            signal(kk,:)=R';
+        end
+    end
+    
+end
