97dcb3f9fc023adb986663bd10353ded563e9044
Hermes Miller <m206305@r5225481.mayo.edu>
2021-04-03 08:39:30 -0500

    adding highpass filter

---
+++ functions/ieeg_highpass.m
@@ -0,0 +1,32 @@
+function [signal_hp] = ieeg_highpass(signal,srate)
+
+parameters.SamplingRate.NumericValue = srate;
+
+% --- highpass filter --- 
+param.highpass.Wp = 0.50; % Hz
+param.highpass.Ws = 0.05; % Hz
+param.highpass.Rp = 3;    % dB
+param.highpass.Rs = 30;   % dB
+
+% define passband, stopband and attenuation
+highpass{1}.Wp = param.highpass.Wp/(parameters.SamplingRate.NumericValue/2); 
+highpass{1}.Ws = param.highpass.Ws/(parameters.SamplingRate.NumericValue/2);
+highpass{1}.Rp = param.highpass.Rp; 
+highpass{1}.Rs = param.highpass.Rs;
+
+% calculate the minimum filter order for butterworth filter
+[highpass{1}.n,highpass{1}.Wn] = buttord(highpass{1}.Wp,highpass{1}.Ws,highpass{1}.Rp,highpass{1}.Rs);
+highpass{1}.n = highpass{1}.n + rem(highpass{1}.n,2);
+
+% calculate the filter coefficients in Zero-Pole-Gain design
+[highpass{1}.z,highpass{1}.p,highpass{1}.k] = butter(highpass{1}.n,highpass{1}.Wn,'high');
+[highpass{1}.sos,highpass{1}.g] = zp2sos(highpass{1}.z,highpass{1}.p,highpass{1}.k);
+highpass{1}.h = dfilt.df2sos(highpass{1}.sos,highpass{1}.g);
+
+signal_hp = zeros(size(signal));
+for idx_channel = 1:size(signal,2)
+    warning('off', 'signal:filtfilt:ParseSOS');
+    signal_hp(:,idx_channel) = single(filtfilt(highpass{1}.sos,highpass{1}.g,double(signal(:,idx_channel))));
+    fprintf(1,'.');    
+end
+fprintf(1,'] done\n');
