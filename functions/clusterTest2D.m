780c67b37aa71b0ccfe3972e6e641a9e3fad13f4
Harvey Huang <huang.harvey@mayo.edu>
2023-08-25 15:50:13 -0500

    clusterTest.m: added output to return the cluster statistics. Small bug fix to prevent cluster statistics accidentally converted to absolute value.\nAdded clusterTest2D.m, which performs the cluster test on matrix inputs, such as spectrograms

--- functions/clusterTest2D.m
+++ functions/clusterTest2D.m
@@ -1,6 +1,6 @@
-%% This function compares time series data for trials across two experimenetal conditions.
-%   A nonparametric test to determine whether clusters of time points exist on which there is a significant difference in means between
-%   two experimental conditions. In brief, clusters of time points that are significantly different between the two conditions are detected first using
+%% This function compares matrix data, such as spectrograms, for trials across two experimental conditions.
+%   A nonparametric test to determine whether contiguous clusters of time-frequency points exist on which there is a significant difference in means between
+%   two experimental conditions. In brief, clusters of pixels that are significantly different between the two conditions are detected first using
 %   2-tailed t-tests on every sample. The sum of the t-statistic in each cluster is then used as a test statistic and compared to a null distribution generated
 %   by permutation testing.
 %
@@ -16,20 +16,20 @@
 %           Journal of Neuroscience, 43(8), 1405-1413.
 %
 %   USAGE:
-%       clusters = clusterTest(x1, x2);                                         % minimal inputs/outputs
-%       [clusters, pSig, tSumsSig] = clusterTest(x1, x2, alphaThresh, alphaFA, nperm);   % full inputs/outputs
-%           x1 =            t x n num. n Trials corresponding to first experimental condition, with t time points to be compared
-%           x2 =            t x m num. m Trials corresponding to second experimental condition.
+%       clusters = clusterTest(s1, s2);                                                     % minimal inputs/outputs
+%       [clusters, pSig, tSumsSig] = clusterTest(s1, s2, alphaThresh, alphaFA, nperm);      % full inputs/outputs
+%           s1 =            f x t x n num. n Trials corresponding to first experimental condition, with f rows (frequencies), t columns (time points)
+%           s2 =            f x t x m num. m Trials corresponding to second experimental condition.
 %           alphaThresh =   (optional) num, 0 < alphaThresh < 1. Alpha of the 2-tailed t-tests to detect samples with significant (uncorrected) difference in means between x1 and x2.
 %                               These samples are clustered on and then subjected to the non-parametric testing. alphaThresh does NOT affect the false alarm (FA)
 %                               rate of the test, but it does affect the sensitivity. Higher alphaThresh detects weaker/longer-lasting effects. Default = 0.05.
 %           alphaFA =       (optional) num, 0 < alphaFA < 1. Alpha (false alarm (FA) rate) of the overall test. Sets the critical values of the two-tailed permutation null distribution.
 %                               This alpha controls the FA rate for ALL clusters tested (see section 3.1.1 last paragraph of Maris & Oostenveld, 2007). Default = 0.05.
-%           nperm =         (optional) num > 0. Number of permutations in null distribution. Default = 1000.
+%           nperm =         (optional) num > 0. Number of permutations in null distribution. Default = 100.
 %
 %   RETURNS:
 %       clusters =          b x 2 num. Each row is the [start, stop] indices in x1, x2 of a significant cluster. There are b significant clusters detected. No significant
-%                               clusters if empty.
+%                               clusters detected if empty.
 %       pSig =              b x 1 num. The permutation p-value for each significant cluster in clusters. Every element of pSig <= alphaFA.
 %       tSumsSig =          b x 1 num. The cluster t-static sum for each significant cluster in clusters.
 %           
@@ -40,51 +40,75 @@
 %       permutation has a max tSum corresponding to a cluster smaller than minimum size and another cluster larger than the minimum size, then setting a minimum
 %       size constraint would change the tSum for that perm from the original to the lower score > minimum size.
 %
+%
+%   EXAMPLE:
+%       
+%       % Create some fake spectrogram data (12 trials in s1, 10 trials in s2)
+%       s1 = randn(20, 200, 12);
+%       s2 = randn(20, 200, 10);
+%       s1(2:4, 120:140, :) = s1(2:4, 120:140, :) + 3; % a positive cluster in s1 relative to s2
+%       s2(11:15, 60:80, :) = s2(11:15, 60:80, :) + 2; % a positive cluster in s2 relative to s1
+%
+%       % find clusters
+%       [clusters, pSig, tSumsSig] = clusterTest2D(s1, s2);
+%
+%       % Plot the spectrograms along with the significant clusters
+%       clustersVisualize = zeros(size(s1, 1), size(s1, 2));
+%       for ii = 1:length(tSumsSig)
+%           clustersVisualize(clusters{ii}) = sign(tSumsSig(ii));
+%       end
+%       subplot(3, 1, 1); imagesc(mean(s1, 3), [-3, 3]); % s1
+%       subplot(3, 1, 2); imagesc(mean(s2, 3), [-3, 3]); % s2
+%       subplot(3, 1, 3); imagesc(clustersVisualize); colormap('gray'); % visualize which clusters are significant
+%
 % HH 08/2023
 %
-function [clusters, pSig, tSumsSig] = clusterTest(x1, x2, alphaThresh, alphaFA, nperm)
+function [clusters, pSig, tSumsSig] = clusterTest2D(s1, s2, alphaThresh, alphaFA, nperm)
     
-    assert(size(x1, 1) == size(x2, 1), 'x1 and x2 must have the same number of rows (samples)');
+    assert(size(s1, 1) == size(s2, 1) && size(s1, 2) == size(s2, 2), 's1 and s2 must have the same first 2 dimensions (image size)');
+    rr = size(s1, 1); cc = size(s2 , 2);
 
-    if nargin < 5, nperm = 1000; end
+    if nargin < 5, nperm = 100; end
     if nargin < 4 || isempty(alphaFA), alphaFA = 0.05; end
     if nargin < 3 || isempty(alphaThresh), alphaThresh = 0.05; end
 
     % number of trials in each condition
-    n = size(x1, 2); m = size(x2, 2);
+    n = size(s1, 3); m = size(s2, 3);
 
     % Get t-statistic critical values for desired alpha1 (to detect which time points are significant)
-    df = size(x1, 2) + size(x2, 2) - 2; % 2-sample ttest df
+    df = size(s1, 3) + size(s2, 3) - 2; % 2-sample ttest df
     crits = tinv([alphaThresh/2, 1 - alphaThresh/2], df); % alpha divided by 2 to reflect 2-tailed critical values on t-distribution.
 
     % calculate t-statistic at each time point
-    tstat = calcTstat(x1, x2);
+    tstat = calcTstat(reshape(s1, [rr*cc, n]), reshape(s2, [rr*cc, m]));
+    tstat = reshape(tstat, [rr, cc]);
     
-    % get the tstatistic sum of all clusters
-    [~, ~, tSums, rgs] = getTsums(tstat, crits);
+    % get the tstatistic sum of all significant clusters in tstat matrix
+    [~, ~, tSums, idxes] = getTsums(tstat, crits);
 
-    if isempty(rgs)
+    if isempty(idxes)
         fprintf('No cluster detected at alpha1 of %0.02f\n', alphaThresh);
         clusters = []; pSig = [];
         return;
     end
 
     % sort tsums in order of decreasing absolute value (greatest differences first)
-    [~, idx] = sort(abs(tSums), 'descend');
-    tSums = tSums(idx);
-    rgs = rgs(idx, :);
+    [~, ii] = sort(abs(tSums), 'descend');
+    tSums = tSums(ii);
+    idxes = idxes(ii);
     
     % Permutation testing to get null distribution
     tSumNull = zeros(nperm, 1); % tSum = 0 for permutations where no cluster above threshold is found
     for ii = 1:nperm
 
         % permute trials between the two conditions
-        xPooled = [x1, x2];
-        xPooled = xPooled(:, randperm(n + m));
-        x1Perm = xPooled(:, 1:n); x2Perm = xPooled(:, n+1:end);
+        sPooled = cat(3, s1, s2);
+        sPooled = sPooled(:, :, randperm(n + m));
+        s1Perm = sPooled(:, :, 1:n); s2Perm = sPooled(:, :, n+1:end);
 
         % get tSumMax as statistic
-        tstatPerm = calcTstat(x1Perm, x2Perm);
+        tstatPerm = calcTstat(reshape(s1Perm, [rr*cc, n]), reshape(s2Perm, [rr*cc, m]));
+        tstatPerm = reshape(tstatPerm, [rr, cc]);
         tSumMaxPerm = getTsums(tstatPerm, crits);
         if ~isempty(tSumMaxPerm), tSumNull(ii) = tSumMaxPerm; end
 
@@ -95,7 +119,7 @@ function [clusters, pSig, tSumsSig] = clusterTest(x1, x2, alphaThresh, alphaFA, 
     for ii = 1:length(p)
         p(ii) = sum(tSumNull >= abs(tSums(ii)) | tSumNull <= -abs(tSums(ii))) / nperm; % area under null more extreme in both tails
     end
-    clusters = rgs(p <= alphaFA, :);
+    clusters = idxes(p <= alphaFA);
     pSig = p(p <= alphaFA);
     tSumsSig = tSums(p <= alphaFA);
 
@@ -104,38 +128,86 @@ end
 
 %% Helper functions
 
-% Input: a vector of t-statistics and two-tailed critical values on the t-distribution, calculated a prior with the correct degrees of freedom.
+% Input: a matrix of t-statistics and two-tailed critical values on the t-distribution, calculated a prior with the correct degrees of freedom.
 % This function calculates the sum of t-statistics (tSum) for contiguous time points with significant tstat (clusters), separately for samples with positive and negative tstat.
 % Returns the tSum corresponding to the maximum cluster (tSumMax) and the cluster [start, stop]. Also returns all tSums and [start, stop] for all clusters
-function [tSumMax, rgMax, tSums, rgs] = getTsums(tstat, crits)
+function [tSumMax, idxMax, tSums, idxes] = getTsums(tstat, crits)
     
     % find significant time points, for positive and negative separately
     hNeg = tstat < crits(1);
     hPos = tstat > crits(2);
 
-    % starts and stops of the positive t-value clusters
-    startsPos = find(diff([0; hPos]) == 1);
-    stopsPos = find(diff([hPos; 0]) == -1);
-
-    % calculate the tstatistic sum of each positive cluster
-    tSumsPos = arrayfun(@(ii) sum(tstat(startsPos(ii):stopsPos(ii))), 1:length(startsPos))';
+    % Get significant positive tstat clusters and calculate sum of each cluster
+    grpsPos = findConnectedGroups(hPos);
+    tSumsPos = cellfun(@(c) sum(tstat(c)), grpsPos);
     
-    % starts and stops of the negative t-value clusters
-    startsNeg = find(diff([0; hNeg]) == 1);
-    stopsNeg = find(diff([hNeg; 0]) == -1);
-
-    % calculate the tstatistic sum of each negative cluster
-    tSumsNeg = arrayfun(@(ii) sum(tstat(startsNeg(ii):stopsNeg(ii))), 1:length(startsNeg))';
+    % Get significant negative tstat clusters and calculate sum of each cluster
+    grpsNeg = findConnectedGroups(hNeg);
+    tSumsNeg = cellfun(@(c) sum(tstat(c)), grpsNeg);
 
     % combine positive and negative clusters into one ranges variable
-    rgs = [startsPos, stopsPos; startsNeg, stopsNeg];
+    idxes = [grpsPos; grpsNeg];
     tSums = [tSumsPos; tSumsNeg];
 
     % find cluster with maximum absolute value cluster sum
-    [~, idx] = max(abs(tSums));
-    rgMax = rgs(idx, :);
-    tSumMax = tSums(idx);
+    [~, ii] = max(abs(tSums));
+    tSumMax = tSums(ii);
+    if isempty(idxes)
+        idxMax = [];
+    else
+        idxMax = idxes{ii};
+    end 
     
+end
+
+% finds connected groups for logical matrix input, using a depth-first search. Assisted by chatGPT in development
+function groups = findConnectedGroups(M)
+
+    M = logical(M); % convert to logical array
+
+    [rr, cc] = size(M);
+    visited = false(rr, cc); % initialize visited matrix (false everywhere)
+    groups = {}; % each cell stores linear indices, to save memory
+    groupIdx = 1;
+
+    for ii = 1:rr
+        for jj = 1:cc
+
+            % current element in matrix is not true or already visited
+            if ~M(ii, jj) || visited(ii, jj), continue; end
+
+            groupMatrix = false(rr, cc);
+            DFS(ii, jj);
+
+            % can consider rejecting single-unit groups
+
+            groups{groupIdx} = find(groupMatrix);
+            groupIdx = groupIdx + 1;
+
+        end
+    end
+
+    % depth-first search
+    function DFS(ii, jj)
+
+        % base case conditions: outside of matrix, element is false or already visited
+        if ii < 1 || ii > rr || jj < 1 || jj > cc || ~M(ii, jj) || visited(ii,jj)
+            return;
+        end
+
+        visited(ii, jj) = true;
+        groupMatrix(ii, jj) = true;
+
+        DFS(ii + 1, jj);
+        DFS(ii - 1, jj);
+        DFS(ii, jj + 1);
+        DFS(ii, jj - 1);
+
+    end
+
+    % ensure column cell vector
+    groups = groups(:);
+
 end
 
 % Calculates the t-statistic for each row of input matrices x1 and x2 (e.g. at each time point of column-wise trials
