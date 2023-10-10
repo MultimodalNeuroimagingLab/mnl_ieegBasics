9511e64ed347f450c1bf94c3f1c11fa97ebb1f1f
dorahermes <dorahermes@gmail.com>
2023-10-10 15:21:09 -0500

    bipolar locs

--- functions/ieeg_Els2BipLocs.m
+++ functions/ieeg_Els2BipLocs.m
@@ -1,13 +1,15 @@
 
-function [bip_loc_info,bip_locs,hemisphere] = ieeg_Els2BipLocs(loc_info,channel_names_bip)
-
+function [bip_loc_info,bip_locs,hemisphere] = ieeg_Els2BipLocs(loc_info,bipolarChans)
+%
+% [bip_loc_info,bip_locs,hemisphere] = ieeg_Els2BipLocs(loc_info,{'LA8-LA9'})
+%
 % Inputs:
 %   loc_info is the BIDS electrodes table, for example:
 %       electrodes_tsv_name = ['sub-' sub_label '_ses-' ses_label '_electrodes.tsv'];
 %       loc_info = readtable(electrodes_tsv_name,'FileType','text','Delimiter','\t','TreatAsEmpty',{'N/A','n/a'});
 %       Needs columns: name, x, y, z, hemisphere and Destrieux label
-% 
-%   channel_names_bip is a cell array of bipolar channel names
+%   bipolarChans = bx1 cell. channel pair names take the form 'ch1-ch2' to represent which pair of channels
+%       are used for the derivation e.g. {'LA1-LA2'}
 %
 % Outputs:
 %
@@ -18,11 +20,12 @@ function [bip_loc_info,bip_locs,hemisphere] = ieeg_Els2BipLocs(loc_info,channel_
 % DH, 2023
 
 % get bipolar positions that match data
-bip_locs = NaN(length(channel_names_bip),3,2); % initialize
-hemisphere = cell(length(channel_names_bip),1);
-Destrieux_label = NaN(length(channel_names_bip),1);
-for kk = 1:length(channel_names_bip)
-    these_els = split(channel_names_bip{kk},'-');
+bip_locs = NaN(length(bipolarChans),3,2); % initialize
+hemisphere = cell(length(bipolarChans),1);
+Destrieux_label = NaN(length(bipolarChans),1);
+Destrieux_label2 = NaN(length(bipolarChans),1);
+for kk = 1:length(bipolarChans)
+    these_els = split(bipolarChans{kk},'-');
     % check length of split - should be 2 electrodes, assuming they do not
     % have a '-' in it
     if length(these_els)==2
@@ -36,10 +39,11 @@ for kk = 1:length(channel_names_bip)
             
             if ismember('Destrieux_label',loc_info.Properties.VariableNames)
                 Destrieux_label(kk) = loc_info.Destrieux_label(el_nr1);
+                Destrieux_label2(kk) = loc_info.Destrieux_label(el_nr2);
             end
         end
     else
-        disp([channel_names_bip{kk} ' not included - missing 2 electrodes']) % checking whether there are 2 electrodes
+        disp([bipolarChans{kk} ' not included - missing 2 electrodes']) % checking whether there are 2 electrodes
     end
 end
 
@@ -48,9 +52,9 @@ end
 bip_centerloc = mean(bip_locs,3);
 
 % create new loc_info structure for bipolar electrodes
-name = channel_names_bip;
+name = bipolarChans;
 x = bip_centerloc(:,1);
 y = bip_centerloc(:,2);
 z = bip_centerloc(:,3);
 
-bip_loc_info = table(name,x,y,z,hemisphere,Destrieux_label);
+bip_loc_info = table(name,x,y,z,hemisphere,Destrieux_label,Destrieux_label2);
